dfb77a843d64d25437c590c2ed268f11d97c1fbe
leodotng <josh@leo.ng>
2018-04-22 16:32:22 -0600

    added database

---
+++ seeds/criticalupdates.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('criticalupdates').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('criticalupdates').insert([
+        {details: 'Its Critical Dude!'},
+        
+      ]);
+    });
+};
