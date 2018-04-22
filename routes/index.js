561cc7a47ff9d0fbe97560ceeb7740b07d8dde3e
leodotng <josh@leo.ng>
2018-04-22 08:46:09 -0600

    updated repo name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CCDP - Command Center Dashboard Pulse' });
 });
 
 module.exports = router;
