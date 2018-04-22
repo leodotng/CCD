76c1c740d48585a8531272ad4ed0447831c92159
leodotng <josh@leo.ng>
2018-04-22 09:00:50 -0600

    changed remote url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CCDP - Command Center Dashboard Pulse' });
+  res.render('index', { title: 'CCD - Command Center Dashboard' });
 });
 
 module.exports = router;
