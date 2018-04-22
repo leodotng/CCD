ebd701fcf05ac91dec5a8a9737a24b6719a5e93d
leodotng <josh@leo.ng>
2018-04-22 15:25:22 -0600

    creation of connection.js

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,16 @@
 // Update with your config settings.
-
+const env = require('dotenv').config()
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
+      localhost: 3000,
+      database: 'swaccd'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  production: ;{
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
