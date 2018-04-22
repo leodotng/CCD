d5e7b96b92dcd1ff5aa451609e12d24002345ed4
leodotng <josh@leo.ng>
2018-04-22 11:46:44 -0600

    added gate change form fields

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CCD - Command Center Dashboard' });
+  res.render('index', { title: 'SWACCD - Command Center Dashboard' });
 });
 
 module.exports = router;
