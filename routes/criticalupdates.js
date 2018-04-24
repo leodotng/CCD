ff4780c0b10a475d1a9bd629031ce1d5ec7f539d
leodotng <josh@leo.ng>
2018-04-23 21:57:39 -0600

    updated route

--- routes/criticalupdates.js
+++ routes/criticalupdates.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../queries');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/criticalupdates', function(req, res, next) {
   db.getAllCriticalUpdates()
   .then(data => res.render('index', {
     data:data
