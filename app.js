3808e89eebbe08136d8d702f4f71911ba4ad828c
leodotng <josh@leo.ng>
2018-04-23 21:59:18 -0600

    fixed router error

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/criticalupdates', criticalupdates)
+app.use('/criticalupdates', criticalupdatesRouter)
 
 
 // catch 404 and forward to error handler
